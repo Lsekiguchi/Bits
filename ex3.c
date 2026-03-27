eb71363ef5e7c937734016fd353f9a2870c549e6
Lsekiguchi <108840988+Lsekiguchi@users.noreply.github.com>
2026-03-27 16:17:07 -0300

    ex3

--- ex3.c
+++ ex3.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 int main() {
-    unsigned int num, loop;
+    unsigned int num, loop, contagem, loop2;
     scanf("%i", &num);
     unsigned int tempos[num];
     loop = 0;
@@ -11,23 +11,29 @@ int main() {
         loop++;
     }
     loop = 0;
+    loop2 = 0;
+    contagem = 1;
     while (loop < num) {
-        if (tempos[loop] <= 5) {
+        while (tempos[loop] > 5) {
+                if (tempos[loop]%5 != 0 && (tempos[loop]/5 + 1)%5 == 0) {
+                    tempos[loop] = tempos[loop]/5 + 1;
+                } else {
+                    tempos[loop] = tempos[loop]/5;
+                }
+                contagem++;
+            loop2++;
+            }
+        tempos[loop] = contagem;
+        contagem = 1;
+        loop2 = 0;
+        loop++;
+    }
+    loop = 0;
+    while (loop < num) {
+        if (tempos[loop] == 1) {
             printf("MIT time\n");
-        }
-        else if (tempos[loop] <= 25) {
-            printf("MIT^2 time\n");
-        }
-        else if (tempos[loop] <= 125) {
-            printf("MIT^3 time\n");
-        }
-        else if (tempos[loop] <= 625) {
-            printf("MIT^4 time\n");
-        }
-        else if (tempos[loop] <= 3125) {
-            printf("MIT^5 time\n");
         } else {
-            printf("MIT^6 time\n");
+            printf("MIT^%i time\n", tempos[loop]);
         }
         loop++;
     }
